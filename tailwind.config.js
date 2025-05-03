f7ab09fab2fe936fc380d98841a66b0029c7a535
Deyan <deyanvalkov>
2025-05-03 02:32:47 +0100

    commit 1

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+// export default {
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "var(--color-primary)",
+//         secondary: "var(--color-secondary)",
+//         accent: "var(--color-accent)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
+// tailwind.config.js
+// import { defineConfig } from "astro/config";
+// import tailwind from "@astrojs/tailwind";
+
+// export default defineConfig({
+//   integrations: [tailwind()],
+// });
+
+// const defaultTheme = require('tailwindcss/defaultTheme');
+
+// module.exports = {
+//   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         orbitron: ['Orbitron', ...defaultTheme.fontFamily.sans],
+//         code: ['Source Code Pro', ...defaultTheme.fontFamily.mono],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+
